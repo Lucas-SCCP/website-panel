0973552ac9c1eecc1e17225a525cb45c6f3fd06e
Lucas da Silva <lucas.2601@gmail.com>
2025-06-28 16:50:39 -0300

    Commit inicial

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import Container from 'react-bootstrap/Container';
+
+export default function Home() {
+  return (
+    <Container className="mt-5 text-center">
+      <h1>Bem-vindo à Home!</h1>
+    </Container>
+  );
+}
