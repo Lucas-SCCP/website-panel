7dde35fb6786c1b08cfe437b47263875788ebfee
Lucas da Silva <lucas.2601@gmail.com>
2025-08-07 22:06:29 -0300

    fix: elemento botao

--- src/elements/ButtonElement.tsx
+++ src/elements/ButtonElement.tsx
@@ -1,45 +1,65 @@
-import { useState } from 'react'
-import { Row, Col, Form, Dropdown, Accordion } from 'react-bootstrap'
+import { Row, Col, Form, Accordion } from 'react-bootstrap'
 import { UseWebsiteStore } from '../stores/UseWebsiteStore'
 import { PropertiesSettings } from '../components/PropertiesSettings'
 import { StylesSettings } from '../components/StylesSettings';
-import type { ComponentType, ElementType, ButtonPropertiesType } from 'website-lib'
+import type { WebsiteType, PageType, ComponentType, ElementType, ButtonPropertiesType } from 'website-lib'
 
 export function ButtonElement({ element }: { element: ElementType }) {
-  const selectedPage = UseWebsiteStore((state) => state.selectedPage)
-  const components = selectedPage?.components.find((c: ComponentType) => c.id === element.component_id)
-  const alerts = components?.elements.content.filter((e: ElementType) => e.element_type_id === 7)
-  const buttons = components?.elements.content.filter((e: ElementType) => e.element_type_id === 8)
+  const selectedWebsite: WebsiteType | null = UseWebsiteStore((state) => state.selectedWebsite)
+  const selectedPage: PageType | null = UseWebsiteStore((state) => state.selectedPage)
+  const component: ComponentType | undefined = selectedPage?.components.find((c) => c.id === element.component_id)
 
-  const properties: ButtonPropertiesType = element.properties as ButtonPropertiesType
+  const updatedElement = component?.elements.content.find((e) => e.id === element.id) || element
 
-  const [type, setType] = useState(properties.type)
-  const [actionId, setAction] = useState(properties.actionId)
-  const [successMessageId, setSuccessMessageId] = useState(properties.successMessageId)
-  const [successActionId, setSuccessActionId] = useState(properties.successActionId)
+  const properties = updatedElement.properties as ButtonPropertiesType
 
-  const handleSelectType = (selectedType: string) => {
-    setType(selectedType)
+  const { updateSelectedElementField } = UseWebsiteStore()
+
+  const setValue = (key: string, file: string) => {
+    if (
+      file == null ||
+      selectedWebsite?.id == null ||
+      selectedPage?.id == null ||
+      component?.id == null
+    ) {
+      return
+    }
+    updateSelectedElementField(
+      component.id,
+      element.id,
+      'properties',
+      {
+        ...updatedElement.properties,
+        [key]: file
+      }
+    )
   }
 
-  const handleSelectAction = (selectedAction: number) => {
-    setAction(selectedAction)
-  }
-
-  const handleSelectSuccessMessageId = (id: number) => {
-    setSuccessMessageId(id)
-  }
-
-  const handleSelectSuccessActionId = (id: number) => {
-    setSuccessActionId(id)
-  }
+  const alerts = component?.elements.content.filter((e: ElementType) => e.element_type_id === 7)
+  const buttons = component?.elements.content.filter((e: ElementType) => e.element_type_id === 8)
 
   return (
     <Row>
       <Col lg={12}>
         <Form.Group className="mb-3" controlId="pageName">
+          <Form.Label>Nome do botão</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Digite o nome do botão"
+            value={properties.name}
+            onChange={(e) => setValue('name', e.target.value)}
+          />
+        </Form.Group>
+      </Col>
+      <Col lg={12}>
+        <Form.Group className="mb-3" controlId="pageName">
           <Form.Label>Texto do botão</Form.Label>
-          <Form.Control type="text" placeholder="Digite o texto do botão" value={properties.title} />
+          <Form.Control
+            type="text"
+            placeholder="Digite o texto do botão"
+            value={properties.title}
+            onChange={(e) => setValue('title', e.target.value)}
+          />
         </Form.Group>
       </Col>
       <Col lg={12}>
@@ -49,92 +69,106 @@ export function ButtonElement({ element }: { element: ElementType }) {
             type="text"
             placeholder="Digite o texto de carregamento do botão"
             value={properties.message}
+            onChange={(e) => setValue('message', e.target.value)}
           />
         </Form.Group>
       </Col>
       <Col lg={6}>
         <Form.Group className="mb-3" controlId="pageName">
           <Form.Label>Tipo de botão</Form.Label>
-          <Dropdown style={{ width: '100%' }}>
-            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic" style={{ width: '100%' }}>
-              {type ? type.charAt(0).toUpperCase() + type.slice(1) : 'Selecione'}
-            </Dropdown.Toggle>
-            <Dropdown.Menu style={{ width: '100%' }}>
-              <Dropdown.Item onClick={() => handleSelectType('submit')}>Submit</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <Form.Select
+            value={properties.type ? properties.type : 'Selecione'}
+            onChange={(e) => setValue('type', e.target.value)}
+            aria-label="Selecione"
+          >
+            <option value="">Selecione</option>
+            <option value="submit">Submit</option>
+            <option value="button">Button</option>
+          </Form.Select>
         </Form.Group>
       </Col>
       <Col lg={6}>
         <Form.Group className="mb-3" controlId="pageName">
           <Form.Label>Ação</Form.Label>
-          <Dropdown style={{ width: '100%' }}>
-            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic" style={{ width: '100%' }}>
-              {actionId ? actionId : 'Selecione'}
-            </Dropdown.Toggle>
-            <Dropdown.Menu style={{ width: '100%' }}>
-              <Dropdown.Item onClick={() => handleSelectAction(1)}>Enviar E-mail</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <Form.Select
+            value={properties.actionId ? properties.actionId : 'Selecione'}
+            onChange={(e) => setValue('actionId', e.target.value)}
+            aria-label="Selecione"
+          >
+            <option value="">Selecione</option>
+            <option value="1">Enviar e-mail</option>
+          </Form.Select>
         </Form.Group>
       </Col>
       <Col lg={6}>
         <Form.Group className="mb-3" controlId="pageName">
           <Form.Label>Mensagem de sucesso</Form.Label>
-          <Dropdown style={{ width: '100%' }}>
-            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic" style={{ width: '100%' }}>
-              {successMessageId
-                ? alerts?.find((alert: ElementType) => alert.id === successMessageId)?.properties.name
-                : 'Selecione'}
-            </Dropdown.Toggle>
-            <Dropdown.Menu style={{ width: '100%' }}>
-              {alerts &&
-                alerts.map((alert: ElementType, index: number) => (
-                  <Dropdown.Item key={index} onClick={() => handleSelectSuccessMessageId(alert.id)}>
-                    {alert.properties.name}
-                  </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-          </Dropdown>
+          <Form.Select
+            value={properties.successMessageId ? properties.successMessageId : 'Selecione'}
+            onChange={(e) => setValue('successMessageId', e.target.value)}
+            aria-label="Selecione"
+          >
+            <option value="">Selecione</option>
+            {alerts && alerts.map((alert: ElementType, index: number) => (
+              <option key={index} value={alert.id}>{alert.properties.name}</option>
+            ))}
+          </Form.Select>
         </Form.Group>
       </Col>
       <Col lg={6}>
         <Form.Group className="mb-3" controlId="pageName">
-          <Form.Label>Ação após clique</Form.Label>
-          <Dropdown style={{ width: '100%' }}>
-            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic" style={{ width: '100%' }}>
-              Selecione
-            </Dropdown.Toggle>
-            <Dropdown.Menu style={{ width: '100%' }}>
-              <Dropdown.Item>Exibir botão</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <Form.Label>Ação de sucesso</Form.Label>
+          <Form.Select
+            value={properties.successActionId ? properties.successActionId : 'Selecione'}
+            onChange={(e) => setValue('successActionId', e.target.value)}
+            aria-label="Selecione"
+          >
+            <option value="">Selecione</option>
+            {buttons && buttons.map((button: ElementType, index: number) => (
+              <option key={index} value={button.id}>{button.properties.name}</option>
+            ))}
+          </Form.Select>
         </Form.Group>
       </Col>
       <Col lg={6}>
         <Form.Group className="mb-3" controlId="pageName">
-          <Form.Label>Selecione botão de ação</Form.Label>
-          <Dropdown style={{ width: '100%' }}>
-            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic" style={{ width: '100%' }}>
-              {successActionId
-                ? buttons?.find((button: ElementType) => button.id === successActionId)?.properties.title
-                : 'Selecione'}
-            </Dropdown.Toggle>
-            <Dropdown.Menu style={{ width: '100%' }}>
-              {buttons &&
-                buttons.map((button: ElementType, index: number) => (
-                  <Dropdown.Item key={index} onClick={() => handleSelectSuccessActionId(button.id)}>
-                    {button.properties.title}
-                  </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-          </Dropdown>
+          <Form.Label>Mensagem de erro</Form.Label>
+          <Form.Select
+            value={properties.errorMessageId ? properties.errorMessageId : 'Selecione'}
+            onChange={(e) => setValue('errorMessageId', e.target.value)}
+            aria-label="Selecione"
+          >
+            <option value="">Selecione</option>
+            {alerts && alerts.map((alert: ElementType, index: number) => (
+              <option key={index} value={alert.id}>{alert.properties.name}</option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </Col>
+      <Col lg={6}>
+        <Form.Group className="mb-3" controlId="pageName">
+          <Form.Label>Ação de erro</Form.Label>
+          <Form.Select
+            value={properties.errorActionId ? properties.errorActionId : 'Selecione'}
+            onChange={(e) => setValue('errorActionId', e.target.value)}
+            aria-label="Selecione"
+          >
+            <option value="">Selecione</option>
+            {buttons && buttons.map((button: ElementType, index: number) => (
+              <option key={index} value={button.id}>{button.properties.name}</option>
+            ))}
+          </Form.Select>
         </Form.Group>
       </Col>
       <Col lg={12}>
         <Form.Group className="mb-3" controlId="pageName">
           <Form.Label>Link</Form.Label>
-          <Form.Control type="text" placeholder="Digite o link do botão" value={properties.path} />
+          <Form.Control
+            type="text"
+            placeholder="Digite o link do botão"
+            value={properties.path}
+            onChange={(e) => setValue('path', e.target.value)}
+          />
         </Form.Group>
       </Col>
       <Col lg={12}>
