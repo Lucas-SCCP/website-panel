16b33a13d2aac7358de9816caeb3c22c2b503289
Lucas da Silva <lucas.2601@gmail.com>
2025-12-08 19:44:04 -0300

    Ajuste vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -6,32 +6,37 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }): UserConfig => {
+  const isDev = command === 'serve'
+  
+  return {
+    plugins: [react()],
 
-  // Permite acessar sua lib local
-  server: {
-    fs: {
-      allow: [
-        // raiz do projeto (ajuste se seu vite estiver em subpasta)
-        path.resolve(__dirname),
-        // pasta da lib (ajuste se necessário)
-        path.resolve(__dirname, '../website-lib'),
-      ]
+    // Permite acessar sua lib local
+    server: isDev
+    ? {
+      fs: {
+        allow: [
+          // raiz do projeto (ajuste se seu vite estiver em subpasta)
+          path.resolve(__dirname),
+          // pasta da lib (ajuste se necessário)
+          path.resolve(__dirname, '../website-lib'),
+        ]
+      }
+    } : undefined,
+
+    // (opcional, mas recomendado) alias para não usar caminhos relativos
+    resolve: {
+      alias: isDev
+      ? {
+        'website-lib': path.resolve(__dirname, '../website-lib/dist')
+        // ou '../website-lib/src' se quiser apontar pro código fonte
+      } : {}
+    },
+
+    // Ajuda o Vite a pré-bundlar dependências importadas pela lib
+    optimizeDeps: {
+      include: ['dompurify', 'react-input-mask']
     }
-  },
-
-  // (opcional, mas recomendado) alias para não usar caminhos relativos
-  resolve: {
-    alias: {
-      'website-lib': path.resolve(__dirname, '../website-lib/dist')
-      // ou '../website-lib/src' se quiser apontar pro código fonte
-    }
-  },
-
-  // Ajuda o Vite a pré-bundlar dependências importadas pela lib
-  optimizeDeps: {
-    include: ['dompurify', 'react-input-mask']
   }
 })
